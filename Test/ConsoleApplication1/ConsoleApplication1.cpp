564b9690ef22fbd271171a25993451e45a43ab62
Pierre Baptiste <pierrebaptiste.demonceaux@viacesi.fr>
2021-10-10 18:31:10 +0200

    First commit

---
+++ Test/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,11 @@
+// ConsoleApplication1.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include "header.h"
+
+int main()
+{
+    dirhello;
+}
+
---
+++ Test/ConsoleApplication1/Header.h
@@ -0,0 +1,7 @@
+#include <iostream>
+
+void dirhello() {
+
+	std::cout << "Hello"; 
+
+}
